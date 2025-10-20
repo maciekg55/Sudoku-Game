a2a2fdbf731ab10a8756bb2781dc4c49339d919a
Maciej Gajdziński <maciejgajdzinski0@gmail.com>
2025-10-20 19:27:52 +0200

    Base version before implementing board initialization

---
+++ Generator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Maciek Gajdziński on 20/10/2025.
+//
+
+#include "Generator.h"
---
+++ Generator.h
@@ -0,0 +1,16 @@
+//
+// Created by Maciek Gajdziński on 20/10/2025.
+//
+
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+
+
+class Generator {
+
+};
+
+
+
+#endif //GENERATOR_H
